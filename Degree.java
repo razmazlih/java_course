ea98231f06ea26ef96aa6889c75c182565ed5f31
Ben FIsher <benfisher6@gmail.com>
2026-05-13 10:53:53 +0300

    Add lecturer model with degree and committees

--- Degree.java
+++ Degree.java
@@ -1,3 +1,16 @@
-public class Degree {
-    
+public enum Degree {
+    FIRST(1),
+    SECOND(2),
+    DOCTOR(3),
+    PROFESSOR(4);
+
+    private int rank;
+
+    Degree(int rank) {
+        this.rank = rank;
+    }
+
+    public boolean isDoctorOrAbove() {
+        return rank >= 3;
+    }
 }
--- Lecturer.java
+++ Lecturer.java
@@ -1,3 +1,41 @@
 public class Lecturer {
-    
+    private String name;
+    private int idNumber;
+    private Degree degree;
+    private String degreeName;
+    private double salary;
+    private Department department;
+    private Committee[] committees;
+    private int committeeCount;
+
+    public Lecturer(String name, int idNumber, Degree degree, String degreeName, double salary) {
+        this.name = name;
+        this.idNumber = idNumber;
+        this.degree = degree;
+        this.degreeName = degreeName;
+        this.salary = salary;
+        this.department = null;
+        this.committees = new Committee[10];
+        this.committeeCount = 0;
+    }
+
+    private void increaseCommitteesArray() {
+        Committee[] newCommittees = new Committee[committees.length * 2];
+
+        for (int i = 0; i < committees.length; i++) {
+            newCommittees[i] = committees[i];
+        }
+
+        committees = newCommittees;
+    }
+
+    public boolean addCommittee(Committee committee) {
+        if (committeeCount == committees.length) {
+            increaseCommitteesArray();
+        }
+
+        committees[committeeCount] = committee;
+        committeeCount++;
+        return true;
+    }
 }
